9a19aca4d15fb82fc892f1acf4159a9ca562c82b
Santiago Bioletto <santiagobioletto@gmail.com>
2024-09-21 17:03:17 -0300

    Feat(Redux) - redux added

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,18 +3,27 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { AppStore, makeStore } from "@/libs/redux/store";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
   return (
-    <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
-        <Toaster position="top-center" />
-        {children}
-      </NextThemesProvider>
-    </NextUIProvider>
+    <Provider store={storeRef.current}>
+      <NextUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light">
+          <Toaster position="top-center" />
+          {children}
+        </NextThemesProvider>
+      </NextUIProvider>
+    </Provider>
   );
 }
