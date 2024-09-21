70c10303b01eb035165bb8b989fad71293e6b2d3
Santiago Bioletto <santiagobioletto@gmail.com>
2024-09-21 19:05:34 -0300

    Fix(Home) - link permissions

--- src/app/components/navbar-items-list.tsx
+++ src/app/components/navbar-items-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardFooter } from "@nextui-org/react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface NavbarItem {
   name: string;
@@ -16,26 +16,26 @@ interface NavbarItemsListProps {
 }
 
 export default function NavbarItemsList({ itemsWithPermission }: NavbarItemsListProps) {
+  const router = useRouter();
   return (
     <div className="gap-6 grid grid-cols-2 pb-4">
       {itemsWithPermission.map((item) => (
-        <Link key={item.name} href={item.href} className="w-full">
-          <Card
-            shadow="sm"
-            key={item.name}
-            isPressable
-            className="pl-10 h-full w-full"
-            isDisabled={!item.hasPermission}
-          >
-            <div className="absolute top-4 left-4">{item.icon}</div>
-            <CardFooter className="flex-col items-start">
-              <b className="text-start text-primary">{item.name}</b>
-              <p className="text-default-500 text-start text-small">
-                {item.description}
-              </p>
-            </CardFooter>
-          </Card>
-        </Link>
+        <Card
+          shadow="sm"
+          key={item.name}
+          isPressable
+          className="pl-10 h-full w-full"
+          isDisabled={!item.hasPermission}
+          onClick={() => item.hasPermission && router.push(item.href)}
+        >
+          <div className="absolute top-4 left-4">{item.icon}</div>
+          <CardFooter className="flex-col items-start">
+            <b className="text-start text-primary">{item.name}</b>
+            <p className="text-default-500 text-start text-small">
+              {item.description}
+            </p>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );
