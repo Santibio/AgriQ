c0c0edaceac0e1c4d4726d73d8e030a0ba93357b
Santiago Bioletto <santiagobioletto@gmail.com>
2024-09-25 20:10:39 -0300

    Feat(General) - allowedOriginis

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "90r9lqhr-3000.brs.devtunnels.ms"],
+    },
+  },
+};
