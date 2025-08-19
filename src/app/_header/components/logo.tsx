fb8b39df73c081fde9225c398d5d8dd3a9258713
Santiago Bioletto <santiagobioletto@gmail.com>
2025-08-19 20:19:01 -0300

    logo fix

--- src/app/_header/components/logo.tsx
+++ src/app/_header/components/logo.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 import { Sprout } from "lucide-react";
-
+import { Link } from "@heroui/react";
 
 export default function Logo() {
   return (
-    <div className="text-xl font-semibold flex items-center gap-2">
-      <Sprout />
-      <h1>
-        Agri
-        <span className="text-primary">Q</span>
-      </h1>
-    </div>
+    <Link href="/" color="foreground">
+      <div className="text-xl font-semibold flex items-center">
+        <Sprout />
+        <h1>
+          Agri
+          <span className="text-primary">Q</span>
+        </h1>
+      </div>
+    </Link>
   );
 }
