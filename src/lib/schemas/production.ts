29f24af0263d15449ac855bcb1f35e3a030d3c94
Santiago Bioletto <santiagobioletto@gmail.com>
2025-11-04 19:35:19 -0300

    fix lote

--- src/lib/schemas/production.ts
+++ src/lib/schemas/production.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const CreateProductionFormSchema = z.object({
   product: z.string({message: "Campo requerido"}).min(1, { message: "Campo requerido" }), // Se usa string().nonempty() para validar que no esté vacío
-  quantity: z.number({message: "Campo requerido"}).min(1, { message: "Campo requerido" }),
+  quantity: z.number({message: "Campo requerido"}).min(1, { message: "Debe ser mayor a 1" }),
 });
 
 export type AddProductionInputs = z.infer<typeof CreateProductionFormSchema>;
